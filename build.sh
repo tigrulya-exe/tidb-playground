05fe054d5223f16878a8efdcfd9d4e79eb4236d8
Yasuo Honda <yahonda@pingcap.com>
2021-12-14 14:20:39 +0900

    Build tidb-playground v5.3.0
    https://docs.pingcap.com/tidb/stable/release-5.3.0

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-TIDB_VERSION_SLICE=('v5.1.1' 'v5.1.0' 'v5.0.3' 'v5.2.0')
+TIDB_VERSION_SLICE=('v5.1.1' 'v5.1.0' 'v5.0.3' 'v5.2.0' 'v5.3.0')
 
 for version in "${TIDB_VERSION_SLICE[@]}"
 do
